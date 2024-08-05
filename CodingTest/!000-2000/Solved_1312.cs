3a18165da834bd786d0780d49910a645234bcbc5
동윤 김 <ut_community@naver.com>
2024-08-06 01:53:55 +0900

    feat : - 1312 Solve

---
+++ CodingTest/!000-2000/Solved_1312.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solved_1312
+{
+    public void Solve()
+    {
+        var splInt = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+        int A = splInt[0];
+        int B = splInt[1];
+        int N = splInt[2];
+
+        var r = A % B;
+        var result = 0;
+        for (int i = 0; i < N; i++)
+        {
+            r *= 10;
+            result = r / B;
+            r %= B;
+        }
+
+        Console.WriteLine(result);
+    }
+}
--- CodingTest/Program.cs
+++ CodingTest/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 
 namespace CodingTest
@@ -9,7 +10,7 @@ namespace CodingTest
     {
         static void Main(string[] args)
         {
-            var s = new Solved_1308();
+            var s = new Solved_1312();
             s.Solve();
         }
     }
